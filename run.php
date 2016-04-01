a13e01bc629053e50daf1a6425119f4df62ec1b1
Thomas Gelf <thomas@gelf.net>
2016-04-01 10:30:49 +0200

    ScreenshotController: workaround for doc images

--- run.php
+++ run.php
@@ -2,3 +2,18 @@
 
 $this->provideHook('director/ImportSource');
 
+// Workaround for doc images:
+if (Icinga::app()->isCli()) {
+    return;
+}
+
+$screenshotRoute = new Zend_Controller_Router_Route(
+    'screenshot/puppetdb/:subdir/:file',
+    array(
+        'module'        => 'puppetdb',
+        'controller'    => 'screenshot',
+    )
+);
+
+$this->addRoute('screenshot/puppetdb', $screenshotRoute);
+
