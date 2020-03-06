08b9c37c0493742bd79ef1c8283110fd1ab3d876
Thomas Gelf <thomas@gelf.net>
2020-03-06 14:49:32 +0100

    BaseApi: split responsibilities

---
+++ library/Puppetdb/BaseApi.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Icinga\Module\Puppetdb;
+
+use InvalidArgumentException;
+use RuntimeException;
+
+class BaseApi
+{
+    /** @var array */
+    protected static $baseUrls = [
+    ];
+
+    /** @var string */
+    protected $version;
+
+    /** @var string */
+    protected $baseUrl;
+
+    /** @var string */
+    protected $pdbHost;
+
+    /** @var string|int */
+    protected $pdbPort;
+
+    /** @var string */
+    protected $configDir;
+
+    /** @var string */
+    protected $certname;
+
+    /**
+     * BaseApi constructor.
+     * @param $version
+     * @param $certname
+     * @param $host
+     * @param int $port
+     */
+    public function __construct($version, $certname, $host, $port = 8081)
+    {
+        $this->setVersion($version);
+        $this->pdbHost  = $host;
+        $this->pdbPort  = $port;
+        $this->certname = $certname;
+    }
+
+    /**
+     * @param $version
+     * @return $this
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+        if (! \array_key_exists($version, self::$baseUrls)) {
+            throw new InvalidArgumentException('Got unknown PuppetDB API version: %s', $version);
+        }
+
+        $this->baseUrl = self::$baseUrls[$version];
+
+        return $this;
+    }
+
+    protected function query($query = null)
+    {
+        if ($query === null) {
+            return '';
+        } else {
+            return $this->encodeParameter('query', $query);
+        }
+    }
+
+    protected function encodeParameter($key, $value)
+    {
+        return $key . '=' . \rawurlencode(json_encode($value));
+    }
+
+    protected function url($url)
+    {
+        return \sprintf('https://%s:%d%s/%s', $this->pdbHost, $this->pdbPort, $this->baseUrl, $url);
+    }
+
+    protected function prepareStreamContext($method, $body = null)
+    {
+        $headers = [
+            'Host: ' . $this->pdbHost . ':8081',
+            'Connection: close'
+        ];
+        if ($body !== null) {
+            $body = \json_encode($body);
+            $headers[] = 'Content-Type: application/json';
+        }
+
+        return [
+            'http' => [
+                'protocol_version' => '1.1',
+                'user_agent'       => 'Icinga Web 2.0 - Director',
+                'method'           => \strtoupper($method),
+                'content'          => $body,
+                'header'           => $headers,
+                'ignore_errors'    => true
+            ],
+            'ssl' => [
+                'peer_name'        => $this->pdbHost,
+                'verify_peer'      => true,
+                'verify_peer_name' => true,
+                'cafile'           => $this->sslDir('certs/ca.pem'),
+                'verify_depth'     => 5,
+                'verify_expiry'    => true,
+                // TODO: re-enable once configurable: 'CN_match'         => $this->pdbHost, // != peer?,
+                'local_cert'       => $this->sslDir('private_keys/' . $this->certname . '_combined.pem'),
+            ]
+        ];
+    }
+
+    protected function request($method, $url, $body = null, $raw = false)
+    {
+        $opts = $this->prepareStreamContext($method, $body);
+        $context = \stream_context_create($opts);
+        $res = \file_get_contents($this->url($url), false, $context);
+        if (\substr(\array_shift($http_response_header), 0, 10) !== 'HTTP/1.1 2') {
+            throw new RuntimeException(\sprintf(
+                'Headers: %s, Response: %s',
+                \implode("\n", $http_response_header),
+                \var_export($res, 1)
+            ));
+        }
+        if ($raw) {
+            return $res;
+        } else {
+            return $res;
+            // return RestApiResponse::fromJsonResult($res);
+        }
+    }
+
+    /**
+     * @param  string $url
+     * @param  string $body
+     * @return string
+     */
+    public function get($url, $body = null)
+    {
+        return $this->request('get', $url, $body);
+    }
+
+    /**
+     * @param  string $url
+     * @param  string $body
+     * @return string
+     */
+    public function getRaw($url, $body = null)
+    {
+        return $this->request('get', $url, $body, true);
+    }
+
+    /**
+     * @param  string $url
+     * @param  string $body
+     * @return string
+     */
+    public function post($url, $body = null)
+    {
+        return $this->request('post', $url, $body);
+    }
+
+    /**
+     * @param  string $sub
+     * @return string
+     */
+    protected function sslDir($sub = null)
+    {
+        return $this->getConfigDir($sub);
+    }
+
+    /**
+     * @param  string $sub
+     * @return string
+     */
+    protected function getConfigDir($sub = null)
+    {
+        if ($this->configDir === null) {
+            $pdb = new PuppetDb();
+            $this->configDir = $pdb->sslDir($this->pdbHost);
+        }
+
+        return $this->configDir . ($sub === null ? '' :  '/' . $sub);
+    }
+}
--- library/Puppetdb/PuppetDbApi.php
+++ library/Puppetdb/PuppetDbApi.php
@@ -3,40 +3,15 @@
 namespace Icinga\Module\Puppetdb;
 
 use Icinga\Data\Filter\Filter;
-use InvalidArgumentException;
-use RuntimeException;
 
-/**
- * Class PuppetDbApi
- * @package Icinga\Module\Puppetdb
- */
-class PuppetDbApi
+class PuppetDbApi extends BaseApi
 {
-    /** @var array */
     protected static $baseUrls = [
         'v1' => '',
         'v2' => '/v2',
         'v3' => '/v3',
         'v4' => '/pdb/query/v4'
     ];
-
-    /** @var string */
-    protected $version;
-
-    /** @var string */
-    protected $baseUrl;
-
-    /** @var string */
-    protected $pdbHost;
-
-    /** @var string|int */
-    protected $pdbPort;
-
-    /** @var string */
-    protected $configDir;
-
-    /** @var string */
-    protected $certname;
 
     /** @var string */
     protected $orderBy;
@@ -50,31 +25,12 @@ class PuppetDbApi
      */
     public function __construct($version, $certname, $host, $port = 8081)
     {
-        $this->setVersion($version);
-        $this->pdbHost  = $host;
-        $this->pdbPort  = $port;
-        $this->certname = $certname;
+        parent::__construct($version, $certname, $host, $port);
         if ($version === 'v4') {
             $this->orderBy = 'order_by';
         } else {
             $this->orderBy = 'order-by';
         }
-    }
-
-    /**
-     * @param $version
-     * @return $this
-     */
-    public function setVersion($version)
-    {
-        $this->version = $version;
-        if (! \array_key_exists($version, self::$baseUrls)) {
-            throw new InvalidArgumentException('Got unknown PuppetDB API version: %s', $version);
-        }
-
-        $this->baseUrl = self::$baseUrls[$version];
-
-        return $this;
     }
 
     /**
@@ -136,11 +92,6 @@ class PuppetDbApi
     protected function orderBy($order)
     {
         return $this->encodeParameter($this->orderBy, $order);
-    }
-
-    protected function encodeParameter($key, $value)
-    {
-        return $key . '=' . \rawurlencode(json_encode($value));
     }
 
     /**
@@ -333,116 +284,5 @@ class PuppetDbApi
     protected function renderFilter(Filter $filter)
     {
         return FilterRenderer::forFilter($filter)->toQueryString();
-    }
-
-    protected function url($url)
-    {
-        return \sprintf('https://%s:%d%s/%s', $this->pdbHost, $this->pdbPort, $this->baseUrl, $url);
-    }
-
-    protected function prepareStreamContext($method, $body = null)
-    {
-        $headers = [
-            'Host: ' . $this->pdbHost . ':8081',
-            'Connection: close'
-        ];
-        if ($body !== null) {
-            $body = \json_encode($body);
-            $headers[] = 'Content-Type: application/json';
-        }
-
-        return [
-            'http' => [
-                'protocol_version' => '1.1',
-                'user_agent'       => 'Icinga Web 2.0 - Director',
-                'method'           => \strtoupper($method),
-                'content'          => $body,
-                'header'           => $headers,
-                'ignore_errors'    => true
-            ],
-            'ssl' => [
-                'peer_name'        => $this->pdbHost,
-                'verify_peer'      => true,
-                'verify_peer_name' => true,
-                'cafile'           => $this->sslDir('certs/ca.pem'),
-                'verify_depth'     => 5,
-                'verify_expiry'    => true,
-                // TODO: re-enable once configurable: 'CN_match'         => $this->pdbHost, // != peer?,
-                'local_cert'       => $this->sslDir('private_keys/' . $this->certname . '_combined.pem'),
-            ]
-        ];
-    }
-
-    protected function request($method, $url, $body = null, $raw = false)
-    {
-        $opts = $this->prepareStreamContext($method, $body);
-        $context = \stream_context_create($opts);
-        $res = \file_get_contents($this->url($url), false, $context);
-        if (\substr(\array_shift($http_response_header), 0, 10) !== 'HTTP/1.1 2') {
-            throw new RuntimeException(\sprintf(
-                'Headers: %s, Response: %s',
-                \implode("\n", $http_response_header),
-                \var_export($res, 1)
-            ));
-        }
-        if ($raw) {
-            return $res;
-        } else {
-            return $res;
-            // return RestApiResponse::fromJsonResult($res);
-        }
-    }
-
-    /**
-     * @param  string $url
-     * @param  string $body
-     * @return string
-     */
-    public function get($url, $body = null)
-    {
-        return $this->request('get', $url, $body);
-    }
-
-    /**
-     * @param  string $url
-     * @param  string $body
-     * @return string
-     */
-    public function getRaw($url, $body = null)
-    {
-        return $this->request('get', $url, $body, true);
-    }
-
-    /**
-     * @param  string $url
-     * @param  string $body
-     * @return string
-     */
-    public function post($url, $body = null)
-    {
-        return $this->request('post', $url, $body);
-    }
-
-    /**
-     * @param  string $sub
-     * @return string
-     */
-    protected function sslDir($sub = null)
-    {
-        return $this->getConfigDir($sub);
-    }
-
-    /**
-     * @param  string $sub
-     * @return string
-     */
-    protected function getConfigDir($sub = null)
-    {
-        if ($this->configDir === null) {
-            $pdb = new PuppetDb();
-            $this->configDir = $pdb->sslDir($this->pdbHost);
-        }
-
-        return $this->configDir . ($sub === null ? '' :  '/' . $sub);
     }
 }
